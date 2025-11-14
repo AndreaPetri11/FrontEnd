b68a7e112f8742fff39d6e214c9f779bac3dd2c4
AndreaPetri11 <andreapetri11@gmail.com>
2025-11-14 16:56:15 +0100

    Interazioni2

---
+++ 14-11/script/script.js
@@ -0,0 +1,11 @@
+
+/*Prompt & Alert
+    let name = prompt("Benvenuto, qual'è il tuo nome?");
+    console.log("Il nome inserito è: " + name);
+    alert("Ciao " + name + ", benvenuto nel Mondo di JavaScript!");
+  */
+
+    function salutaUtente() {
+  let nomeUtente = document.getElementById("nomeUtente").value;
+  alert("Ciao " + nomeUtente + "!");
+}
