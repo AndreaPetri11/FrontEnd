4ca852b2114f2015ec3c1f687f3020c1396d25e5
AndreaPetri11 <andreapetri11@gmail.com>
2025-11-17 12:42:52 +0100

    Esercizio_Mini_Calcolatrice

---
+++ Esercitazioni_Front_End/Mini_Calcolatrice_17-11-25/script.js
@@ -0,0 +1,31 @@
+
+function calcola() {
+let n1 = Number(document.getElementById("n1").value);
+let n2 = Number(document.getElementById("n2").value);
+let somma = n1 + n2;
+	document.getElementById("risultato").textContent = "Risultato: " + somma;
+   
+}
+
+function sottrai() {
+let n1 = Number(document.getElementById("n1").value);
+let n2 = Number(document.getElementById("n2").value);
+let sottrai = n1 - n2;
+	document.getElementById("risultato").textContent = "Risultato: " + sottrai;
+   
+}
+
+function moltiplica() {
+let n1 = Number(document.getElementById("n1").value);
+let n2 = Number(document.getElementById("n2").value);
+let moltiplica = n1 * n2;
+	document.getElementById("risultato").textContent = "Risultato: " + moltiplica;
+   
+}
+
+function dividi() {
+let n1 = Number(document.getElementById("n1").value);
+let n2 = Number(document.getElementById("n2").value);
+let dividi = n1 / n2;
+	document.getElementById("risultato").textContent = "Risultato: " + dividi;   
+}
