8a43354929a46c2cfdc681adbee13a9bf01b9a88
AndreaPetri11 <andreapetri11@gmail.com>
2025-11-27 12:45:06 +0100

    esempi lezione

--- 27-11/src/App.jsx
+++ 27-11/src/App.jsx
@@ -1,10 +1,27 @@
 import WelcomeCard from "./components/WelcomeCard/WelcomeCard";
 import "./App.css";
+import { Saluta } from "./components/Hello/Hello";
 
 function App() {
+  const users = [
+    { id: 1, name: "Lorenzo", age: 18 },
+    { id: 2, name: "Giovanni", age: 13 },
+    { id: 3, name: "Gianluca", age: 35 },
+  ];
+
   return (
     <>
       <WelcomeCard name="Gino" age={18} isVip={true} />
+      <br></br>
+      <br></br>
+      <Saluta name="Mario" age={30} />
+      <Saluta name="Carlo" age={13} />
+      <Saluta name="Luca" age={35} />
+      <br></br>
+      <br></br>
+      {users.map((user) => (
+        <Saluta key={user.id} name={user.name} age={user.age} />
+      ))}
     </>
   );
 }
---
+++ 27-11/src/components/Hello/Hello.jsx
@@ -0,0 +1,7 @@
+export function Saluta({ name, age }) {
+  return (
+    <p>
+      Ciao {name}, hai {age} anni!
+    </p>
+  );
+}
