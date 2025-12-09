0d6ed604fb1ad721abc7a72bf92810453bf66c31
AndreaPetri11 <andreapetri11@gmail.com>
2025-12-09 18:03:15 +0100

    generateStaticParams

--- esercitazione_nextjs_09_12/src/app/guides/[...path]/page.jsx
+++ esercitazione_nextjs_09_12/src/app/guides/[...path]/page.jsx
@@ -1,10 +1,21 @@
 import { guides } from "@/lib/guides";
+import { notFound } from "next/navigation";
+
+export function generateStaticParams() {
+  return guides.map((guide) => ({ path: guide.path }));
+}
 
 export default async function GuidePage({ params }) {
   const { path } = await params;
   const pathVerify = path.join("/");
-  const guide = guides.find((guide) => guide.path.join("/") === pathVerify);
+  const guide = guides.find(
+    (guide) =>
+      guide.path.length === path.length && guide.path.join("/") === pathVerify
+  );
 
+  if (!guide) {
+    notFound();
+  }
   return (
     <div>
       <h1>{guide.title} </h1>
