930c2cad24601fd21cf32e4b2c3f65eccb7e0ed3
AndreaPetri11 <andreapetri11@gmail.com>
2025-12-09 16:13:40 +0100

    metodo 2 inserimento immagine

--- esercitazione_nextjs_09_12/src/app/not-found.js
+++ esercitazione_nextjs_09_12/src/app/not-found.js
@@ -1,11 +1,17 @@
 import Image from "next/image";
-//import doh from "../assets/doh.jpg";
+//metodo 2//import doh from "../../public/doh.jpg";
 
 export default function Notfound() {
   return (
     <>
       <p>"Pagina non trovata"</p>
-      <Image src="/doh.jpg" height={250} width={200} alt="doh" />
+      <Image
+        src="/doh.jpg"
+        //src={doh} Metodo 2
+        height={250}
+        width={200}
+        alt="doh"
+      />
     </>
   );
 }
