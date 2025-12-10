e626431bb66fbc8d3e2d12737cfbbf2826616a04
AndreaPetri11 <andreapetri11@gmail.com>
2025-12-10 12:21:48 +0100

    funzionante

--- 10-12/src/app/movies/page.jsx
+++ 10-12/src/app/movies/page.jsx
@@ -1,17 +1,19 @@
 import { getPopular } from "@/lib/tmdb";
 
-const data = await getPopular();
+export default async function MoviePage() {
+  const data = await getPopular();
 
-return (
-  <section>
-    <h1>Film Popolari:</h1>
-    <div>
-      {" "}
-      {data.results.map((movie) => (
-        <article key={movie.id}>
-          <h2>Titolo: {movie.title}</h2>
-        </article>
-      ))}
-    </div>
-  </section>
-);
+  return (
+    <section>
+      <h1>Film Popolari:</h1>
+      <div>
+        {" "}
+        {data.results.map((movie) => (
+          <article key={movie.id}>
+            <h2>Titolo: {movie.title}</h2>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+}
